f447b802313243088ae310aff0023a5c4c1bfd06
Kehinde Falode (TMNA) <kehinde.falode@toyota.com>
2024-06-18 14:11:46 -0400

    create a login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'loader',
     loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 
 @NgModule({
